4bacf660eef1000fb0f1fc8b8dfc6fa4cc4d5134
[mishra gaurav] <vishwajeetsingh@tecraki.io>
2024-12-09 22:50:49 +0530

    add new  code

--- src/Components/Layout/Dashboard.jsx
+++ src/Components/Layout/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../BasicComponents/Navbar';
-import ShirtImage from '../../../src/assets/img/dashhead1.jpg'
+import tshirts from '../../../src/assets/img/tshirt_15197861.png'
+import shirtimage1 from '../../../src/assets/img/shirt_2405655.png'
+import sneakers from '../../../src/assets/img/sneakers_1692750.png'
 
 
 const Dashboard = () => {
@@ -10,45 +12,77 @@ const Dashboard = () => {
     <div className="container-fluid">
      <div className="d-flex justify-content-center pagecolor">
       <div className="row">
-        <div className="col-md">
-          <img src={ShirtImage}
-          className="img-fluid " 
-          height={10}
+        <div className="col-md text-center mt-5 me-5">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
             alt="" 
           />
-          <h1>Welcome to your Dashboard</h1>
+          <h5>Shirts</h5>
         </div>
-        <div className="col-md">
-          <img src={ShirtImage}
-          className="img-fluid " 
-          height={10}
+        <div className="col-md text-center mt-5 me-5">
+          <img src={tshirts}
+          className="img-fluid mb-2" 
+          width={50}
             alt="" 
           />
-          <h1>Welcome to your Dashboard</h1>
+          <h5>Shirts</h5>
         </div>
-        <div className="col-md">
-          <img src={ShirtImage}
-          className="img-fluid " 
-          height={10}
+        <div className="col-md text-center mt-5 me-5">
+          <img src={sneakers}
+          className="img-fluid mb-2" 
+          width={50}
             alt="" 
           />
-          <h1>Welcome to your Dashboard</h1>
+          <h5>Shirts</h5>
         </div>
-        <div className="col-md">
-          <img src={ShirtImage}
-          className="img-fluid " 
-          height={10}
+        <div className="col-md text-center mt-5 me-5">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
             alt="" 
           />
-          <h1>Welcome to your Dashboard</h1>
+          <h5>Shirts</h5>
         </div>
-        <div className="col-md">
-          <img src={ShirtImage}
-          className="img-fluid " 
-          height={10}
+        <div className="col-md text-center mt-5 me-5">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
             alt="" 
           />
-          <h1>Welcome to your Dashboard</h1>
+          <h5>Shirts</h5>
+        </div>
+        <div className="col-md text-center mt-5 me-5">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
+            alt="" 
+          />
+          <h5>Shirts</h5>
+        </div>
+        <div className="col-md text-center mt-5 me-5">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
+            alt="" 
+          />
+          <h5>Shirts</h5>
+        </div>
+        <div className="col-md text-center mt-5 me-5">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
+            alt="" 
+          />
+          <h5>Shirts</h5>
+        </div>
+        <div className="col-md text-center mt-5 me-5 mb-3">
+          <img src={shirtimage1}
+          className="img-fluid mb-2" 
+          width={50}
+            alt="" 
+          />
+          <h5>Shirts</h5>
         </div>
       </div>
      </div>
